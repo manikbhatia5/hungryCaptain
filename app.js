3534cd551f2067942407c18f550e4aa579c6cd3c
manikbhatia5 <103627725+manikbhatia5@users.noreply.github.com>
2022-05-16 09:07:49 +0530

    all files

---
+++ app.js
@@ -0,0 +1,16 @@
+// const axios = require("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://burgers1.p.rapidapi.com/burgers',
+//   headers: {
+//     'X-RapidAPI-Host': 'burgers1.p.rapidapi.com',
+//     'X-RapidAPI-Key': '59242823c6msh9a2c3e2f648cf14p15a224jsn7227743b53db'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
